1e93205b05d66e06f6e6ecf0098d1cdde6ce35ba
Rinat Khusainov <rinat.khusainov.msk@gmail.com>
2019-10-26 21:59:22 +0300

    Add winner logic and show result

--- app/src/main/java/com/khusainov/rinat/tictactoe/MainActivity.java
+++ app/src/main/java/com/khusainov/rinat/tictactoe/MainActivity.java
@@ -1,15 +1,21 @@
 package com.khusainov.rinat.tictactoe;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static int count = 0;
+    private static int clickCounter = 0;
 
     private Button mTopLeftButton;
     private Button mTopCenterButton;
@@ -20,12 +26,18 @@ public class MainActivity extends AppCompatActivity {
     private Button mBottomLeftButton;
     private Button mBottomCenterButton;
     private Button mBottomRightButton;
+    private List<View> mButtons;
+    private TextView mCrossCount;
+    private TextView mCircleCount;
+    private View mResultLayout;
+    private TextView mWinnerTextView;
 
-
-    boolean gameOver = false;
-    int[][] ticTacToe = new int[3][3];
-    int winner = -1;
-    int xo = -1;
+    private static int crossWins = 0;
+    private static int circleWins = 0;
+    private boolean gameOver = false;
+    private int gridSize = 3;
+    private int[][] ticTacToe = new int[gridSize][gridSize];
+    private int xo = -1;
 
 
     @Override
@@ -33,19 +45,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mResultLayout = findViewById(R.id.result_layout);
+        mWinnerTextView = findViewById(R.id.tv_winner);
+        mCrossCount = findViewById(R.id.tv_cross_count);
+        mCircleCount = findViewById(R.id.tv_circle_count);
+
+        initGrid();
         initButtons();
-//        TicTacToeField ticTacToeField = new TicTacToeField(3);
-//        ticTacToeField.setFigure();
+        setResult();
+    }
 
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+    private void initGrid() {
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
                 ticTacToe[i][j] = -1;
             }
         }
     }
 
     private void initButtons() {
+        mButtons = new ArrayList<>();
+
         mTopLeftButton = findViewById(R.id.topLeftButton);
         mTopCenterButton = findViewById(R.id.topCenterButton);
         mTopRightButton = findViewById(R.id.topRightButton);
@@ -56,208 +76,231 @@ public class MainActivity extends AppCompatActivity {
         mBottomCenterButton = findViewById(R.id.bottomCenterButton);
         mBottomRightButton = findViewById(R.id.bottomRightButton);
 
-        mTopLeftButton.setOnClickListener(new View.OnClickListener() {
-            int flag = 0;
+        mButtons.add(mTopLeftButton);
+        mButtons.add(mTopCenterButton);
+        mButtons.add(mTopRightButton);
+        mButtons.add(mLeftCenterButton);
+        mButtons.add(mCenterButton);
+        mButtons.add(mRightCenterButton);
+        mButtons.add(mBottomLeftButton);
+        mButtons.add(mBottomCenterButton);
+        mButtons.add(mBottomRightButton);
+    }
 
-            @Override
-            public void onClick(View view) {
-                if (flag == 0) {
-                    if (count % 2 == 0) {
-                        mTopLeftButton.setBackground(getResources().getDrawable(R.drawable.circle));
-                        xo = 0;
+    public void pushXO(View view) {
+        if (!gameOver) {
+            Button button = (Button) view;
+            Drawable drawable = button.getBackground();
 
-                    } else {
-                        mTopLeftButton.setBackground(getResources().getDrawable(R.drawable.cross));
-                        xo = 1;
-                    }
+            // Проверка на второе нажатие по кнопке
+            if (drawable.getConstantState() == getResources().getDrawable(R.drawable.circle).getConstantState() ||
+                    drawable.getConstantState() == getResources().getDrawable(R.drawable.cross).getConstantState()) {
+                Toast.makeText(this, "Выберите другую клетку!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            // Первый клик X, второй - 0
+            if (clickCounter % 2 == 0) {
+                button.setBackground(getResources().getDrawable(R.drawable.cross));
+                xo = 1;
+            } else {
+                button.setBackground(getResources().getDrawable(R.drawable.circle));
+                xo = 0;
+            }
+
+            int buttonId = 0;
+            switch (button.getId()) {
+                case R.id.topLeftButton:
                     ticTacToe[0][0] = xo;
-                }
-                flag = 1;
-                count++;
-            }
-        });
-
-        mTopCenterButton.setOnClickListener(new View.OnClickListener() {
-            int flag = 0;
-
-            @Override
-            public void onClick(View view) {
-                if (flag == 0) {
-                    if (count % 2 == 0) {
-                        mTopCenterButton.setBackground(getResources().getDrawable(R.drawable.circle));
-                        xo = 0;
-
-                    } else {
-                        mTopCenterButton.setBackground(getResources().getDrawable(R.drawable.cross));
-                        xo = 1;
-
-                    }
+                    buttonId = 1;
+                    Log.i("Button click: ", "0 0");
+                    break;
+                case R.id.topCenterButton:
                     ticTacToe[1][0] = xo;
-                }
-                flag = 1;
-                count++;
-            }
-        });
-
-
-        mTopRightButton.setOnClickListener(new View.OnClickListener() {
-            int flag = 0;
-
-            @Override
-            public void onClick(View view) {
-                if (flag == 0) {
-                    if (count % 2 == 0) {
-                        mTopRightButton.setBackground(getResources().getDrawable(R.drawable.circle));
-                        xo = 0;
-
-                    } else {
-                        mTopRightButton.setBackground(getResources().getDrawable(R.drawable.cross));
-                        xo = 1;
-                    }
+                    buttonId = 2;
+                    Log.i("Button click: ", "1 0");
+                    break;
+                case R.id.topRightButton:
                     ticTacToe[2][0] = xo;
-                }
-                flag = 1;
-                count++;
-            }
-        });
-
-
-        mLeftCenterButton.setOnClickListener(new View.OnClickListener() {
-            int flag = 0;
-
-            @Override
-            public void onClick(View view) {
-                if (flag == 0) {
-                    if (count % 2 == 0) {
-                        mLeftCenterButton.setBackground(getResources().getDrawable(R.drawable.circle));
-                        xo = 0;
-
-                    } else {
-                        mLeftCenterButton.setBackground(getResources().getDrawable(R.drawable.cross));
-                        xo = 1;
-                    }
+                    buttonId = 3;
+                    Log.i("Button click: ", "2 0");
+                    break;
+                case R.id.leftCenterButton:
                     ticTacToe[0][1] = xo;
-                }
-                flag = 1;
-                count++;
-            }
-        });
-
-
-        mCenterButton.setOnClickListener(new View.OnClickListener() {
-            int flag = 0;
-
-            @Override
-            public void onClick(View view) {
-                if (flag == 0) {
-                    if (count % 2 == 0) {
-                        mCenterButton.setBackground(getResources().getDrawable(R.drawable.circle));
-                        xo = 0;
-
-                    } else {
-                        mCenterButton.setBackground(getResources().getDrawable(R.drawable.cross));
-                        xo = 1;
-                    }
+                    buttonId = 4;
+                    Log.i("Button click: ", "0 1");
+                    break;
+                case R.id.centerButton:
                     ticTacToe[1][1] = xo;
-                }
-                flag = 1;
-                count++;
-            }
-        });
-
-        mRightCenterButton.setOnClickListener(new View.OnClickListener() {
-            int flag = 0;
-
-            @Override
-            public void onClick(View view) {
-                if (flag == 0) {
-                    if (count % 2 == 0) {
-                        mRightCenterButton.setBackground(getResources().getDrawable(R.drawable.circle));
-                        xo = 0;
-                    } else {
-                        mRightCenterButton.setBackground(getResources().getDrawable(R.drawable.cross));
-                        xo = 1;
-                    }
-                    ticTacToe[1][1] = xo;
-                }
-                flag = 1;
-                count++;
-            }
-        });
-
-
-        mBottomLeftButton.setOnClickListener(new View.OnClickListener() {
-            int flag = 0;
-
-            @Override
-            public void onClick(View view) {
-                if (flag == 0) {
-                    if (count % 2 == 0) {
-                        mBottomLeftButton.setBackground(getResources().getDrawable(R.drawable.circle));
-                        xo = 0;
-
-                    } else {
-                        mBottomLeftButton.setBackground(getResources().getDrawable(R.drawable.cross));
-                        xo = 1;
-                    }
+                    buttonId = 5;
+                    Log.i("Button click: ", "1 1");
+                    break;
+                case R.id.rightCenterButton:
+                    ticTacToe[2][1] = xo;
+                    buttonId = 6;
+                    Log.i("Button click: ", "2 1");
+                    break;
+                case R.id.bottomLeftButton:
                     ticTacToe[0][2] = xo;
-
-                }
-                flag = 1;
-                count++;
-            }
-        });
-
-
-        mBottomCenterButton.setOnClickListener(new View.OnClickListener() {
-            int flag = 0;
-
-            @Override
-            public void onClick(View view) {
-                if (flag == 0) {
-                    if (count % 2 == 0) {
-                        mBottomCenterButton.setBackground(getResources().getDrawable(R.drawable.circle));
-                        xo = 0;
-
-                    } else {
-                        mBottomCenterButton.setBackground(getResources().getDrawable(R.drawable.cross));
-                        xo = 1;
-                    }
+                    buttonId = 7;
+                    Log.i("Button click: ", "0 2");
+                    break;
+                case R.id.bottomCenterButton:
                     ticTacToe[1][2] = xo;
-
-                }
-                flag = 1;
-                count++;
-            }
-        });
-
-
-        mBottomRightButton.setOnClickListener(new View.OnClickListener() {
-            int flag = 0;
-
-            @Override
-            public void onClick(View view) {
-                if (flag == 0) {
-
-                    if (count % 2 == 0) {
-                        mBottomRightButton.setBackground(getResources().getDrawable(R.drawable.circle));
-                        xo = 0;
-
-                    } else {
-                        mBottomRightButton.setBackground(getResources().getDrawable(R.drawable.cross));
-                        xo = 1;
-                    }
+                    buttonId = 8;
+                    Log.i("Button click: ", "1 2");
+                    break;
+                case R.id.bottomRightButton:
                     ticTacToe[2][2] = xo;
-                }
-                flag = 1;
-                count++;
-
+                    buttonId = 9;
+                    Log.i("Button click: ", "2 2");
+                    break;
             }
-        });
 
-        if (count == 9) {
-            Toast.makeText(this, "Game Over! No winner!", Toast.LENGTH_SHORT).show();
+            // Игра закончена
+            if (gameOver(buttonId)) {
+                gameOver = true;
+                showGameResult();
+                switch (xo) {
+                    case 1:
+                        mWinnerTextView.setText(getResources().getString(R.string.cross));
+                        crossWins++;
+                        break;
+                    case 0:
+                        mWinnerTextView.setText(getResources().getString(R.string.circle));
+                        circleWins++;
+                        break;
+                }
+                setResult();
+            }
+
+            if (++clickCounter == 9) {
+                gameOver = true;
+                mWinnerTextView.setText(getResources().getString(R.string.draw));
+                showGameResult();
+            }
+        }
+    }
+
+    public void showGameResult() {
+        mResultLayout.setVisibility(View.VISIBLE);
+    }
+
+    public boolean gameOver(int buttonId) {
+        switch (buttonId) {
+            case 1:
+                if (ticTacToe[0][0] == ticTacToe[0][1] && ticTacToe[0][0] == ticTacToe[0][2]) {
+                    return true;
+                }
+                if (ticTacToe[0][0] == ticTacToe[1][0] && ticTacToe[0][0] == ticTacToe[2][0]) {
+                    return true;
+                }
+                if (ticTacToe[0][0] == ticTacToe[1][1] && ticTacToe[0][0] == ticTacToe[2][2]) {
+                    return true;
+                }
+                break;
+            case 2:
+                if (ticTacToe[1][0] == ticTacToe[0][0] && ticTacToe[1][0] == ticTacToe[2][0]) {
+                    return true;
+                }
+                if (ticTacToe[1][0] == ticTacToe[1][1] && ticTacToe[1][0] == ticTacToe[1][2]) {
+                    return true;
+                }
+                break;
+            case 3:
+                if (ticTacToe[2][0] == ticTacToe[0][0] && ticTacToe[2][0] == ticTacToe[1][0]) {
+                    return true;
+                }
+                if (ticTacToe[2][0] == ticTacToe[1][1] && ticTacToe[2][0] == ticTacToe[0][2]) {
+                    return true;
+                }
+                if (ticTacToe[2][0] == ticTacToe[2][1] && ticTacToe[2][0] == ticTacToe[2][2]) {
+                    return true;
+                }
+                break;
+            case 4:
+                if (ticTacToe[0][1] == ticTacToe[0][0] && ticTacToe[0][1] == ticTacToe[0][2]) {
+                    return true;
+                }
+                if (ticTacToe[0][1] == ticTacToe[1][1] && ticTacToe[0][1] == ticTacToe[2][1]) {
+                    return true;
+                }
+                break;
+            case 5:
+                if (ticTacToe[1][1] == ticTacToe[0][0] && ticTacToe[1][1] == ticTacToe[2][2]) {
+                    return true;
+                }
+                if (ticTacToe[1][1] == ticTacToe[2][0] && ticTacToe[1][1] == ticTacToe[0][2]) {
+                    return true;
+                }
+                if (ticTacToe[1][1] == ticTacToe[1][0] && ticTacToe[1][1] == ticTacToe[1][2]) {
+                    return true;
+                }
+                if (ticTacToe[1][1] == ticTacToe[0][1] && ticTacToe[1][1] == ticTacToe[2][1]) {
+                    return true;
+                }
+                break;
+            case 6:
+                if (ticTacToe[2][1] == ticTacToe[2][0] && ticTacToe[2][1] == ticTacToe[2][2]) {
+                    return true;
+                }
+                if (ticTacToe[2][1] == ticTacToe[0][1] && ticTacToe[2][1] == ticTacToe[1][1]) {
+                    return true;
+                }
+                break;
+            case 7:
+                if (ticTacToe[0][2] == ticTacToe[1][2] && ticTacToe[0][2] == ticTacToe[2][2]) {
+                    return true;
+                }
+                if (ticTacToe[0][2] == ticTacToe[1][1] && ticTacToe[0][2] == ticTacToe[2][0]) {
+                    return true;
+                }
+                if (ticTacToe[0][2] == ticTacToe[0][0] && ticTacToe[0][2] == ticTacToe[0][1]) {
+                    return true;
+                }
+                break;
+            case 8:
+                if (ticTacToe[1][2] == ticTacToe[0][2] && ticTacToe[1][2] == ticTacToe[2][2]) {
+                    return true;
+                }
+                if (ticTacToe[1][2] == ticTacToe[1][1] && ticTacToe[1][2] == ticTacToe[1][0]) {
+                    return true;
+                }
+                break;
+            case 9:
+                if (ticTacToe[2][2] == ticTacToe[1][2] && ticTacToe[2][2] == ticTacToe[0][2]) {
+                    return true;
+                }
+                if (ticTacToe[2][2] == ticTacToe[1][1] && ticTacToe[2][2] == ticTacToe[0][0]) {
+                    return true;
+                }
+                if (ticTacToe[2][2] == ticTacToe[2][1] && ticTacToe[2][2] == ticTacToe[2][0]) {
+                    return true;
+                }
+                break;
         }
 
+        return false;
+    }
+
+    private void setResult() {
+        mCrossCount.setText(String.valueOf(crossWins));
+        mCircleCount.setText(String.valueOf(circleWins));
+    }
+
+    public void restartGame(View view) {
+        initButtons();
+        initGrid();
+        clickCounter = 0;
+        gameOver = false;
+        mResultLayout.setVisibility(View.GONE);
+
+        for (View btn : mButtons) {
+            btn.setBackground(getResources().getDrawable(R.drawable.button_stroke));
+        }
+    }
+
+    public void quitGame(View view) {
+        this.finish();
     }
 }
